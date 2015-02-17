c2c14b94bbac6ed8cef6829f148dcf7593584859
hemed <hemed_ali@hotmail.com>
2015-02-17 20:38:27 +0100

    modify setTest method

--- src/edu/stanford/smi/protegex/widget/identifier/UBBIdentifierWidget.java
+++ src/edu/stanford/smi/protegex/widget/identifier/UBBIdentifierWidget.java
@@ -4,38 +4,43 @@ import java.util.Collection;
 
 import edu.stanford.smi.protege.model.*;
 import edu.stanford.smi.protege.util.CollectionUtilities;
-import edu.stanford.smi.protege.util.SystemUtilities;
 import edu.stanford.smi.protege.widget.TextFieldWidget;
-import java.util.Date;
 import java.util.UUID;
 
 /**
- * Copies the value of the system property "user.name" into a string slot.
- *
- * @author Phillip Cheng
- * @version 1.1
+ * @author  Hemed Ali
  */
 
 public class UBBIdentifierWidget extends TextFieldWidget {
         
 	public static boolean isSuitable(Cls cls, Slot slot, Facet facet) {
     	boolean isSuitable;
-    	if ((cls == null) || (slot == null)) {
-      		isSuitable = false;
-      	} else {
+    	//if ((cls == null) || (slot == null)) {
+      	//	isSuitable = false;
+      	//} else {
       		//boolean isAnyType = cls.getTemplateSlotValueType(slot) == ValueType.ANY;
       		boolean isMultiple = cls.getTemplateSlotAllowsMultipleValues(slot);
       		isSuitable = !isMultiple;
-    	}
+    	//}
 		return isSuitable;
 	}
         
         @Override
-	public void setValues(final Collection values) {
-          String uniqueId = UUID.randomUUID().toString();
-       if(this.getText().isEmpty())
-          this.setText(uniqueId);
-  	}
+          public void setValues(final Collection values) {
+            String uniqueId = (String)CollectionUtilities.getFirstItem(values);
+            
+            if (uniqueId == null){
+                
+               //generate unique Id 
+               uniqueId = UUID.randomUUID().toString();
+              
+               setText(uniqueId);
+               getTextField().setEnabled(false);
+               setInstanceValues();
+             } else {
+              super.setValues(values);
+            }
+          }
 
        /** @Override
 	public void setValues(final Collection values) {
