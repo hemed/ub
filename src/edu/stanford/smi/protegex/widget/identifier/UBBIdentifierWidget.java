dfbed218c4a38e680ea2a0dad0024718ab54ee2c
Hemed Ali <hemed_ali@hotmail.com>
2015-02-18 17:19:14 +0100

    Update UBBIdentifierWidget.java

--- src/edu/stanford/smi/protegex/widget/identifier/UBBIdentifierWidget.java
+++ src/edu/stanford/smi/protegex/widget/identifier/UBBIdentifierWidget.java
@@ -25,22 +25,28 @@ public class UBBIdentifierWidget extends TextFieldWidget {
 		return isSuitable;
 	}
         
-        @Override
-          public void setValues(final Collection values) {
-            String uniqueId = (String)CollectionUtilities.getFirstItem(values);
-            
-            if (uniqueId == null){
+            public void setValues(final Collection values) {
+              String uniqueId = (String)CollectionUtilities.getFirstItem(values);
+              Instance ins = getInstance();
+             
+              if (uniqueId == null){
+               String s2 = ins.getName().replaceAll("([A-Z][a-z]+)_[0-9]+$","instance/person/").toLowerCase();
+               
+                //generate unique Id
+                uniqueId = UUID.randomUUID().toString();
+                setText(uniqueId);
+                //getTextField().setEnabled(false);
+                setInstanceValues();
                 
-               //generate unique Id 
-               uniqueId = UUID.randomUUID().toString();
-              
-               setText(uniqueId);
-               getTextField().setEnabled(false);
-               setInstanceValues();
-             } else {
-              super.setValues(values);
-            }
-          }
+                System.out.println("++++++++++++++++++++++\n" + s2 + uniqueId);
+                System.out.println("Direct Type" + ins.getDirectType());
+                System.out.print("Frame name: " + ins.getFrameID().getName());
+                //ins.rename(s2 + uniqueId);
+               
+              }
+           else 
+             super.setValues(values);
+        }
 
        /** @Override
 	public void setValues(final Collection values) {
